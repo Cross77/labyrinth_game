11e462a99e95c7b0f093076c289347754b637620
Jeremiasz Mazur <jeremiasz.mazur@movity.pl>
2018-06-24 09:48:02 +0200

    init

---
+++ Assets/IntensyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntensyController : MonoBehaviour
+{
+
+    public Text Counter;
+    private string Label = "INT: ";
+    private int Intensity = 100;
+    private float minWaitTime = 1;
+    private float maxWaitTime = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            Intensity--;
+            Counter.text = Label + Intensity;
+            //testLight.enabled = !testLight.enabled;
+            //testLight.intensity = Random.Range(minIntensity, maxIntensity);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Battery : MonoBehaviour {
+
+    private Text Counter;
+
+    public string Label = "BTR: ";
+
+    void Start() {
+        Counter = GameObject.FindGameObjectWithTag("GUICounter").GetComponent<Text>();
+        Counter.text = Label + leaveBattery();
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Counter.text = "Baterie: " + leaveBattery();
+            Destroy(gameObject);
+            //return;
+        }
+        /*
+        if (leaveBattery() == 1)
+        {
+            Debug.Log("Zakonczono poziom");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }*/
+    }
+
+    int leaveBattery()
+    {
+        Battery[] batteries = Component.FindObjectsOfType<Battery>();
+        return batteries.Length;
+    }
+}
